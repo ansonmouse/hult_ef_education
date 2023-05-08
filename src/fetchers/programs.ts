2ce3c15095054268784aa610175df13bd76844c0
Anson Kwan <anson.kwan@codebox.hk>
2023-05-09 00:05:10 +0100

    :sparkles: Include ability to set backend url as environment variable

--- src/fetchers/programs.ts
+++ src/fetchers/programs.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { Program } from '../interfaces/Program';
 
+const url =
+  process.env.REACT_APP_PROGRAMS_URL || 'http://localhost:3010/programs';
+
 export const programsFetchPrograms = async () => {
-  const responses = await axios.get('http://localhost:3010/programs');
+  const responses = await axios.get(url);
   const results: Program[] = responses.data;
 
   return results;
